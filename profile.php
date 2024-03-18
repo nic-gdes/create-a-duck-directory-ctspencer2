de8c92b09474565fe76a2912693e77ef41d573ef
ctspencer2 <118775383+ctspencer2@users.noreply.github.com>
2024-03-17 20:57:00 -0700

    Submission for Assignment 9.1

--- profile.php
+++ profile.php
@@ -1,8 +1,30 @@
+<?php
+
+$duck_is_live
+
+if (isset($_GET['id']))
+    $id = htmlspecialchars($_GET['id']);
+
+        require('./config/db.php');
+
+        $sql = "SELECT id, name, favorite_foods, bio, img_src FROM ducks WHERE id=$id";
+        $result = mysqli_query($conn, $sql);
+
+        $duck = mysqli_fetch_assoc($result);
+
+        mysqli_free_result($result);
+        mysqli_close($conn);
+
+?>
+
+
+
+
 <?php include('components/head.php'); ?>
 <?php include('components/nav.php'); ?>
 <main>
     <section>
-        <img src="./assets/images/duck-profile.jpg" alt="Duck Name">
+        <img src="<?php echo $duck['img_src']; ?>" alt="Duck Name">
         <h1>Duck Name</h1>
         <ul>
             <li>Favorite Food 1</li>
